9178ea6c8e231a7eb138e6e610e081862304dde2
Edu Caubilla <educaubilla@gmail.com>
2025-03-05 09:23:01 +0100

    Initial Commit

---
+++ BreakingQuotes/BreakingQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingQuotesApp.swift
+//  BreakingQuotes
+//
+//  Created by Edu Caubilla on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct BreakingQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreakingQuotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BreakingQuotes
+//
+//  Created by Edu Caubilla on 5/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
